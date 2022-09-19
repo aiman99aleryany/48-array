32ea3a92ace64cc3718cec884d384ffd31b92676
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-19 18:06:43 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,31 @@
+// Create an array called students. This array must have 3 objects literals with the following properties:
+
+// id
+// name
+// surname
+// age
+// Print the first element of the array.
+
+const students = [
+  {
+    id: 1,
+    name: "Aiman",
+    surname: "Al Eryany",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Jason",
+    surname: "Stan",
+    age: 22,
+  },
+
+  {
+    id: 3,
+    name: "Mazen",
+    surname: "Venzo",
+    age: 20,
+  },
+];
+
+console.log(students[0]);
